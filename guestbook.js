eee0f76a592605e83d3d82b81a3caeb8698de539
Dr. Robi Mostafa <101678736+robihsnm@users.noreply.github.com>
2022-04-28 23:28:45 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2b80152e.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
